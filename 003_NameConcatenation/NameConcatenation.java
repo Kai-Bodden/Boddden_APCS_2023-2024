eb9f27ece7e6891c472b717484d240f6d5ec5e96
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-12 17:59:59 -0500

    wrote my name by concatenating

---
+++ 003_NameConcatenation/NameConcatenation.java
@@ -0,0 +1,10 @@
+public class NameConcatenation {
+    public static void main(String[]args) {
+        String first = " My";
+        String second = " name";
+        String third = " is";
+        String fourth = " Kai";
+        String fifth = " Bodden";
+        System.out.println(first + second + third + fourth + fifth);
+    }
+}
