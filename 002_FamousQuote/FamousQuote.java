d2c864cb990ce5b60bca35c97393e1ffa0757045
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-12 17:41:39 -0500

    Add files via upload

---
+++ 002_FamousQuote/FamousQuote.java
@@ -0,0 +1,6 @@
+public class FamousQuote {
+    public static void main(String[]args) {
+        System.out.println("\"c'mon man...that's too easy\"");
+        System.out.println("                             -Lebron James");
+    }
+}
