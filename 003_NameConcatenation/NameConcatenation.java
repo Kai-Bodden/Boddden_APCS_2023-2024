5849f7eadb87f92a07d64ed4bdf06059da5110b4
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-13 13:49:09 -0500

    changed name concatenation

--- 003_NameConcatenation/NameConcatenation.java
+++ 003_NameConcatenation/NameConcatenation.java
@@ -1,10 +1,6 @@
 public class NameConcatenation {
     public static void main(String[]args) {
-        String first = " My";
-        String second = " name";
-        String third = " is";
-        String fourth = " Kai";
-        String fifth = " Bodden";
-        System.out.println(first + second + third + fourth + fifth);
+    
+        System.out.println(" My"+ " name" + " is" + " Kai" + " Bodden");
     }
 }
