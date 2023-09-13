5e112b810da58bea7ab4078e99ef2ad8b2146375
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-13 15:20:28 -0500

    movie stats kai

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,13 @@
+public class MediaStats {
+   public static void main(String[]args) {
+   int a=2015;
+   int b=2;
+   int c=2014;
+   int d=3;
+   int e=2020;
+   int f=2;
+        System.out.println("The movie San Andreas, was released in "+""+a+" with a run time of "+""+b+" hours");
+        System.out.println("The movie Interstellar, was released in "+""+c+" with a run time of "+""+d+" hours");
+        System.out.println("The movie Soul, was released in "+""+e+" with a run time of "+""+f+" hours");
+   }
+}
