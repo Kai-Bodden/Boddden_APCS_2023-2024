a42bb3eea40fd71af73cf57ad5711c002529d61f
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-15 16:16:43 -0500

    updated tempflux done

--- 006_TemperatureFlux/TemperatureFlux.java
+++ 006_TemperatureFlux/TemperatureFlux.java
@@ -2,8 +2,16 @@ public class TemperatureFlux {
     public static void main(String[]args) {
         int tempHighNum = (73);
         int tempLowNum = (51);
-        int tempDiffnNum = (tempHighNum - tempLowNum);
-            String tempDifference = (" The difference between the high and low temperature is " + tempDiffnNum
+        //int math example
+        int tempDiffNum = (tempHighNum - tempLowNum);
+        //concatenations and adding variables to create a larger variable
+        //strings helpful instead of printing because it is easier to add to as the variable becomes more complicated
+            String tempDifference = ("The difference between the high and low temperature is " + tempDiffNum + " degrees.");
+            String tempHigh = ("Today's High: " + tempHighNum + " degrees.");
+            String tempLow = ("Today's Low: " + tempLowNum + " degrees.");   
+                System.out.println(tempHigh);
+                System.out.println(tempLow);
+                System.out.println(tempDifference);
              
             
 
