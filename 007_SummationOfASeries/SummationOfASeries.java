48dfb7d50a228c4781e2f65fce3bdf6fffe23489
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-15 16:33:56 -0500

    finished sum of series

---
+++ 007_SummationOfASeries/SummationOfASeries.java
@@ -0,0 +1,18 @@
+public class SummationOfASeries {
+    public static void main(String [] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5; 
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        //adding multiple ints 
+        //must be int variable, ints cannot convert to strings
+        int sum = (a + b + c + d + e + f + g + h +i);
+            System.out.println(sum);
+    
+    }
+}
