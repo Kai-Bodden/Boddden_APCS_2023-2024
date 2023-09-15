fca5575721966caf716e349b8ed76abea0d6cff8
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-14 20:25:07 -0500

    overcomplicated media stats

--- 004_MediaStats/MediaStats.java
+++ 004_MediaStats/MediaStats.java
@@ -1,13 +1,30 @@
 public class MediaStats {
+
    public static void main(String[]args) {
-   int a=2015;
-   int b=2;
-   int c=2014;
-   int d=3;
-   int e=2020;
-   int f=2;
-        System.out.println("The movie San Andreas, was released in "+""+a+" with a run time of "+""+b+" hours");
-        System.out.println("The movie Interstellar, was released in "+""+c+" with a run time of "+""+d+" hours");
-        System.out.println("The movie Soul, was released in "+""+e+" with a run time of "+""+f+" hours");
+
+      int releaseA=2015;
+      int runTimeA=2;
+      int releaseB=2014;
+      int runTimeB=3;
+      int releaseC=2020;
+      int runTimeC=2;
+ 
+         String sanAn = ("San Andreas");
+         String inter = ("Interstellar");
+         String soul = ("Soul");
+         String movieA = ("The movie " + sanAn + " was released in " + releaseA + " with a run time of " + runTimeA + " hours.");
+         String movieB = ("The movie " + inter + " was released in " + releaseB + " with a run time of " + runTimeB + " hours.");
+         String movieC = ("The movie " + soul + " was released in " + releaseC + " with a run time of " + runTimeC + " hours.");
+ 
+            System.out.println(movieA);
+            System.out.println(movieB);
+            System.out.println(movieC);
+
+               /* 
+               //less complicated version :/
+               System.out.println("The movie San Andreas was released in "+""+releaseA+" with a run time of "+""+runTimeA+" hours");
+               System.out.println("The movie Interstellar was released in "+""+releaseB+" with a run time of "+""+runTimeB+" hours");
+               System.out.println("The movie Soul was released in "+""+releaseC+" with a run time of "+""+runTimeC+" hours");
+               */
    }
 }
