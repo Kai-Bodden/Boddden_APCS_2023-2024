ed284bd9668901e2adfe3edec5e95a766076e309
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-14 20:01:14 -0500

    created and finished table of variables project

---
+++ 005_ATableOfVariables/ATableOfVariables.java
@@ -0,0 +1,21 @@
+public class ATableOfVariables {
+    public static void main(String[] args) {
+        int c1r1 =1;
+        int c1r2 =2;
+        int c1r3 =3;
+        int c1r4 =4;
+        int c2r1 =1;
+        int c2r2 =4;
+        int c2r3 =9;
+        int c2r4 =16;
+        int c3r1 =1;
+        int c3r2 =8;
+        int c3r3 =27;
+        int c3r4 =64;
+            System.out.println("a    a^2    a^3");
+            System.out.println(c1r1 + "    " + c2r1 + "      " + c3r1);
+            System.out.println(c1r2 + "    " + c2r2 + "      " + c3r2);
+            System.out.println(c1r3 + "    " + c2r3 + "      " + c3r3);
+            System.out.println(c1r4 + "    " + c2r4 + "     " + c3r4);
+    }
+}
