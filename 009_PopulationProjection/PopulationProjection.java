bf984d1a2bfbb1570c6c089945963e0744a752eb
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-18 18:30:43 -0500

    completed population projection

--- 009_PopulationProjection/PopulationProjection.java
+++ 009_PopulationProjection/PopulationProjection.java
@@ -5,24 +5,27 @@ public class PopulationProjection {
         int dR = (13);
         int iR = (45);
         int year = (365);
-        int day = (24);
         //86400 sec in a day
-        int yearlyBR = (86400/bR) * year * day;
-        int yearlyDR = (86400/dR) * year * day;
-        int yearlyIR = (86400/iR) * year * day;
+        int yearlyBR = (86400/bR) * year;
+        int yearlyDR = (86400/dR) * year;
+        int yearlyIR = (86400/iR) * year;
         int yearlyPopChg = yearlyBR - yearlyDR + yearlyIR;
         int firstYear = currentPop + yearlyPopChg;
         int secondYear = firstYear + yearlyPopChg;
         int thirdYear = secondYear + yearlyPopChg;
         int fourthYear = thirdYear + yearlyPopChg;
         int fifthYear = fourthYear + yearlyPopChg;
+        String printYearOne = ("Year one: ");
+        String printYearTwo = ("Year two: ");
+        String printYearThree = ("Year Three: ");
+        String printYearFour = ("Year Four: ");
+        String printYearFive = ("Year Five: ");
 
-        System.out.println(firstYear);
-        System.out.println(secondYear);
-        System.out.println(yearlyPopChg);
-        
-        
-
+        System.out.println(printYearOne + firstYear);
+        System.out.println(printYearTwo + secondYear);
+        System.out.println(printYearThree + thirdYear);
+        System.out.println(printYearFour + fourthYear);
+        System.out.println(printYearFive + fifthYear);
     }
     
 }
