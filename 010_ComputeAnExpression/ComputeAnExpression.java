50ae782b3040d53a6bc8639b2fd5c2c1c90370a2
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-09-20 14:36:30 -0500

    finished

---
+++ 010_ComputeAnExpression/ComputeAnExpression.java
@@ -0,0 +1,16 @@
+public class ComputeAnExpression {
+    public static void main (String [] args) {
+
+        double first = 9.5;
+        double second = 4.5;
+        double third = 2.5;
+        double fourth = 3.0;
+        double fifth = 45.5;
+        double sixth = 3.5;
+        double equation = (first * second - third * fourth) / (fifth - sixth);
+        System.out.println(equation);
+    
+    }
+}
+    
+
