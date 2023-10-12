028ac40e012cefebf76c6afd936d399c64744ab1
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-10-12 14:45:18 -0500

    started

---
+++ 018_LorumIpsumStringMet/LorumIpsumStringMethods.java
@@ -0,0 +1,6 @@
+public class LorumIpsumStringMethods {
+    public static void main(String[]args) {
+        String lorumIpsum = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Tellus orci ac auctor augue mauris augue neque gravida. Convallis tellus id interdum velit laoreet id. Nibh venenatis cras sed felis eget velit aliquet sagittis. Volutpat blandit aliquam etiam erat velit. Lectus vestibulum mattis ullamcorper velit sed. Nisl pretium fusce id velit. Non blandit massa enim nec dui nunc mattis enim. Felis eget velit aliquet sagittis id. Sapien eget mi proin sed. Metus dictum at tempor commodo ullamcorper a. Morbi blandit cursus risus at.");
+        NameTransmogrifier transmogrifier = new NameTransmogrifier(lorumIpsum); 
+    }
+}
