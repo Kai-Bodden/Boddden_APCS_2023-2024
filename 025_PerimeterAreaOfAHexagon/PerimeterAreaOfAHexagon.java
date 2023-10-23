35ef7b52d7d3ee33015ef6887ce9aabf5f3d4481
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-10-23 15:43:01 -0500

    done

--- 025_PerimeterAreaOfAHexagon/PerimeterAreaOfAHexagon.java
+++ 025_PerimeterAreaOfAHexagon/PerimeterAreaOfAHexagon.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 public class PerimeterAreaOfAHexagon {
     public static void main (String [] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello, what is the length of your circle? ");
+        System.out.println("Hello, what is the side length of your hexagon? ");
+        double sideLength = scan.nextDouble();
+        double perimeter = (6 * sideLength);
+        double area = (((3 * Math.sqrt(3)) / 2) * Math.pow(sideLength, 2.0));
+        System.out.println("The perimeter of your hexagon is: " + perimeter);
+        System.out.println("The area of your hexagon is: " + area); 
+//math.sqrt helped as well as knowing pemdas
     }
     
 }
