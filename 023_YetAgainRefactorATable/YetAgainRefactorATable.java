a6c4e5373348ce1cf6db15d5cb0fd66a0187e823
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-10-23 14:22:29 -0500

    done

---
+++ 023_YetAgainRefactorATable/YetAgainRefactorATable.java
@@ -0,0 +1,22 @@
+public class YetAgainRefactorATable {
+    public static void main(String[]args) { 
+        double c1r1 = 1.0;
+        double c1r2 = 2.0;
+        double c1r3 = 3.0;
+        double c1r4 = 4.0;
+        double c2r1 = Math.pow(c1r1, 2.0);
+        double c2r2 = Math.pow(c1r2, 2.0);
+        double c2r3 = Math.pow(c1r3, 2.0);
+        double c2r4 = Math.pow(c1r4, 2.0);
+        double c3r1 = Math.pow(c1r1, 3.0);
+        double c3r2 = Math.pow(c1r2, 3.0);
+        double c3r3 = Math.pow(c1r3, 3.0);
+        double c3r4 = Math.pow(c1r4, 3.0);
+
+        System.out.println("a    a^2    a^3");
+        System.out.println(c1r1 + "    " + c2r1 + "      " + c3r1);
+        System.out.println(c1r2 + "    " + c2r2 + "      " + c3r2);
+        System.out.println(c1r3 + "    " + c2r3 + "      " + c3r3);
+        System.out.println(c1r4 + "    " + c2r4 + "     " + c3r4);
+    }
+}
