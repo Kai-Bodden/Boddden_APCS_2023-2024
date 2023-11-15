f744d9bd6bef5c2675e7df128b99478eb74b593d
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-11-15 13:45:44 -0600

    jhyymhmjy j

--- 033_RPS/RPSOne.java
+++ 033_RPS/RPSOne.java
@@ -5,19 +5,37 @@ public class RPSOne {
         int num = (int) ((Math.random()*3)+1);
         System.out.println("Rock, Paper, or Scissors?");
         String user = scan.nextLine();
-        
+        String comp = "";
         if (num == 1)
         {
-            String comp = "rock";
+            comp = "rock";
         }
-        else if (num == 2)
+            else if (num == 2) 
+            {
+            comp = "paper";
+            }
+                else
+                {
+                comp = "scissors";
+                }
+        if (!user.equalsIgnoreCase("rock") && !user.equalsIgnoreCase("paper") && !user.equalsIgnoreCase("scissors"))
         {
-            String comp = "paper";
+            System.out.println("Invalid choice.");
         }
-        else if (num == 3)
+        else if (user.equalsIgnoreCase(comp))
         {
-            String comp = "scissors";
+            System.out.println("The computer chose:" + comp);
+            System.out.println("tie :(");
         }
-            if (comp = user)
-    }
+        else if ((user.equalsIgnoreCase("rock") && comp.equals("scissors")) || (user.equalsIgnoreCase("paper") && comp.equals("rock")) || (user.equalsIgnoreCase("scissors") && comp.equals("paper")))
+        {
+            System.out.println("The computer chose:" + comp);
+            System.out.println("You won!");
+        }
+        else
+        {
+            System.out.println("The computer chose:" + comp);
+            System.out.println("You lost:(");
+        }   
+	}
 }
