569743ac164e8c4f713c0096b9e298800361a087
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-11-15 14:19:24 -0600

    jejwrjrgmt

---
+++ 034_UnethicalRPS/UnethicalRPS.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class UnethicalRPS {
+    public static void main (String[]args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Rock, Paper, or Scissors.");
+        String user = scan.nextLine();
+        String rock = "Rock";
+        String paper = "Paper";
+        String scissors ="Scissors";
+
+        if (!user.equalsIgnoreCase(rock) && !user.equalsIgnoreCase(paper) && !user.equalsIgnoreCase(scissors))
+        {
+            System.out.println("Invalid choice :(");
+        }
+        else if (user.equalsIgnoreCase(rock))
+        {
+            System.out.println("The computer chose Paper, you lose."); 
+        }
+        else if (user.equalsIgnoreCase(paper))
+        {
+            System.out.println("The computer chose Scissors, you lose.");
+        }
+        else
+        {
+            System.out.println("The computer chose Rock, you lose.");
+        }
+    }
+}
