4fefe699dd86b86c4593f62c547e83cc3d368d8b
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-11-30 14:43:16 -0600

    the investigation is going downhill

---
+++ 040_PowerAttack/PowerAttack.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PowerAttack {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your trying to avoid having to be in calc ab with Liam McLay, but the scary math monster wants to drag you down. Fight to survive.");
+        int strength = 3;
+    }
+}
