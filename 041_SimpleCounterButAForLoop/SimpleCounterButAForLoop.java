33a534052903b3313fe3e700e95ca7f5d41f4377
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-12-05 14:10:12 -0600

    gfhjhkj

---
+++ 041_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -0,0 +1,21 @@
+public class SimpleCounterButAForLoop {
+    public static void main(String[] args) {
+        for (int count1 = 0; count1 <= 300; count1 ++)
+        {
+            System.out.println(count1);
+        }
+        for (int count2 = 300; count2 >= 0; count2 --)
+        {
+            System.out.println(count2);
+        }
+        for (int count3 = 4798; count3 <= 5324; count3 ++)
+        {
+            System.out.println(count3);
+        }
+        for (int count4 = 5324; count4 >= 4798; count4 --)
+        {
+            System.out.println(count4);
+        }
+        //for (initiallize, condition, directions)
+    }   
+}
