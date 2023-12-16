6de235c05486fa92680126d17630fc607070b5ae
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2023-12-15 21:40:45 -0600

    dgrggwgfqf

--- 048_DoubloonWords/DoubloonWords.java
+++ 048_DoubloonWords/DoubloonWords.java
@@ -1,58 +1,28 @@
-/*import java.util.Scanner;
-public class DoubloonWords{
-    public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please provide a word:");
-        String user = scan.nextLine();
-        String trim = user.trim();
-        String word = trim.toLowerCase();
-
-        boolean Doubloon = false;
-
-        for(int i=0; i<word.length(); i++){
-        int count = 0;
-                for(int k=i+1; k<word.length(); k++){
-                    if(word.substring(i, i+1) == word.substring(k, k+1)){
-                        count++;
-                    }
-                }
-                if(count > 0){
-                    Doubloon = true;
-                }
-        }
-        if(Doubloon == true){
-            System.out.println(user + " is a Doubloon");
-        }
-        else{
-            System.out.println(user + " is not a Doubloon");
-        }
-
-    }
-}
-*/
 import java.util.Scanner;
-
-public class DoubloonWords {
-
-    public static void main(String[] args) {
-        // Accept user input
+public class DoubloonWords{
+    public static void main(String[] args){
+        //user input
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a word: ");
-        String user = scan.nextLine();
-        user = user.toLowerCase();
-        for (int i = 0; i < user.length(); i++) {
+        String input = scan.nextLine();
+        String user = input.toLowerCase();
+       
+        for (int i = 0; i < user.length(); i++){
             String a = user.substring(i, i + 1);
-                for (int j = 0; j < user.length(); j++) {
-                    String b = user.substring(j, j + 1);
-                    if (b.equalsIgnoreCase(a)) {
-                        count++;
-                    }
-                }
-                if (count != 2) {
-                    System.out.println(userInput + " is not a doubloon word.");
-                    return;
+            int count = 0;
+            //will go through each letter, comparing to the first letter, then will continue
+            for (int j = 0; j < user.length(); j++){
+                String b = user.substring(j, j + 1);
+                if (b.equalsIgnoreCase(a)){
+                    count++;
                 }
             }
+            //Check if the current letter appears twice
+            if (count != 2){
+                System.out.println(user + " is not a doubloon word.");
+                return; //Exit the program if not a doubloon word
+            }
         }
-        System.out.println(userInput + " is a doubloon word!");
+        System.out.println(user + " is a doubloon word!");
+    }
 }
