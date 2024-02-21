ff54f3a67ce5f1341a08fa5974050cdfbbaef4b7
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-02-21 13:49:20 -0600

    nngneheh

--- 064_PhoneTwo/HandTwo.java
+++ 064_PhoneTwo/HandTwo.java
@@ -1,13 +1,15 @@
 public class HandTwo {
     public static void main(String[] args) {
+        //making phones
         PhoneTwo phoneA = new PhoneTwo();
         PhoneTwo phoneB = new PhoneTwo("Samsung", 10, "mini", "Orange", "USB-C", 69);
         PhoneTwo phoneC = new PhoneTwo("Huaweii", 20, "Max");
 
+        //toString
         System.out.println(phoneA.toString());
         System.out.println(phoneB.toString());
         System.out.println(phoneC.toString());
-
+`
         phoneA.text("Texting from my new iPhone 15");
         phoneB.music("Freestyle - Lil Baby");
         phoneC.call("Incoming call from: MOM");
