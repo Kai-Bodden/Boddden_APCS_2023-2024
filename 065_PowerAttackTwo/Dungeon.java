26cf3b6a0fd239a0c22f64e249dc7e23b5243d24
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-02-28 14:20:02 -0600

    ,kjj

--- 065_PowerAttackTwo/Dungeon.java
+++ 065_PowerAttackTwo/Dungeon.java
@@ -2,17 +2,40 @@ public class Dungeon{
     public static void main(String[] args) {
         System.out.println("You are brought into a dungeon with a monster.");
 
-        Hero hero = new Hero("Lebron", 100, 50, 700);
-        Monster monster = new Monster("Gorlock", "Destroyer", 7000, 3000, 10);
+        Hero hero = new Hero("Lebron", 100, 11, 700);
+        Monster monster = new Monster("Gorlock", "Destroyer", 5000, 701, 10);
 
         hero.heroDesc();
         monster.monsterDesc();
 
-        if(monster.getHealth()>0){
-            System.out.println("Lebron begins his attack.");
-            System.out.println("His attack has a value of: " + hero.getAttack());
-        }
-    
+        while(monster.getHealth() >= 0 && hero.getHealth() >= 0){
+            if(hero.getAttack()>monster.getArmor()){
+            System.out.println(hero.getName() + " attacks");
+            System.out.println("The attack has a value of: " + hero.getAttack());
+            monster.setHealth(monster.getHealth()-hero.getAttack());
+            System.out.println("The monsters health is now " + monster.getHealth());
+            }
+            else{
+                System.out.println("The hero's attack is ineffective.");
+            }
+            if(monster.getAttack()>hero.getArmor()){
+            System.out.println(monster.getName() + " attacks");
+            System.out.println("The attack has avalue of: " + monster.getAttack());
+            hero.setHealth(hero.getHealth() - monster.getAttack());
+            System.out.println("The heros health is now " + hero.getHealth());
+            }
+            else{
+                System.out.println("The monster's attack is ineffective.");
+            }
+
+            if(monster.getHealth()<0){
+                System.out.println(hero.getName() + " wins!");
+            }
+            else if(hero.getHealth()<0){
+                System.out.println(monster.getName() + " wins!");
+            }
+        
+       }
     }
 }
 
