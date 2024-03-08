72faa758acb9af71627da5d00a8374c0b4d6c18e
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-03-08 12:24:31 -0600

    gvnbnnthtbht

--- 068_TransactionReport/AccountingComputer.java
+++ 068_TransactionReport/AccountingComputer.java
@@ -1,3 +1,12 @@
 public class AccountingComputer {
-    
+    public static void main(String[] args) {
+       CashRegister registerOne = new CashRegister();
+       TransactionReport report = new TransactionReport();
+
+       report.calcTotalCost(registerOne.createTransactions());
+       report.calcTotalProfit(registerOne.createTransactions());
+       report.calcTotalMargin(registerOne.createTransactions());
+       report.calcTotalSalePrice(registerOne.createTransactions());
+        
+    }
 }
--- 068_TransactionReport/TransactionReport.java
+++ 068_TransactionReport/TransactionReport.java
@@ -1,7 +1,35 @@
 public class TransactionReport {
-    public void totalCost(Transaction[] arr){
+    private double totalCost = 0;
+    private double totalSalePrice = 0;
+    private double totalProfit = 0;
+    private double totalMargin = 0;
+
+    public void calcTotalCost(Transaction[] arr){
         for (int i = 0; i < arr.length; i++){
-            
+            totalCost = totalCost + arr[i].getItemCost(); 
         }
+        System.out.println("Total cost of: $" + totalCost);
     }
+
+    public void calcTotalSalePrice(Transaction[] arr){
+        for (int i = 0; i < arr.length; i++){
+            totalSalePrice = totalSalePrice + arr[i].getItemSalePrice();
+        }
+        System.out.println("Total sale price of: $" + totalSalePrice);
+    }
+
+    public void calcTotalProfit(Transaction[] arr){
+        for (int i = 0; i < arr.length; i++){
+            totalProfit = totalProfit + arr[i].getItemProfit();
+        }
+        System.out.println("Total proft of: $" + totalProfit);
+    }
+
+    public void calcTotalMargin(Transaction[] arr){
+        for (int i = 0; i < arr.length; i++){
+            totalMargin = totalMargin + arr[i].getItemMargin();
+        }
+        System.out.println("Total sale price of: $" + totalMargin);
+    }
+
 }
