68f9b9213b683793e00fb7a006dedb00376aa8ee
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-03-08 08:58:25 -0600

    oo j

--- 068_TransactionReport/Transaction.java
+++ 068_TransactionReport/Transaction.java
@@ -4,14 +4,37 @@ public class Transaction {
     private double itemCost;
     private double itemSalePrice;
     private double itemProfit;
-    private double itemMargin;
 
-    public Transaction(String itemName, String itemDesc, double itemCost, double itemSalePrice, double itemProfit, double itemMargin){
+    public Transaction(String itemName, String itemDesc, double itemCost, double itemSalePrice){
         this.itemName = itemName;
         this.itemDesc = itemDesc;
         this.itemCost = itemCost;
         this.itemSalePrice = itemSalePrice;
-        this.itemProfit = itemProfit;
-        this.itemMargin = itemMargin;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+    public String getItemDesc(){
+        return itemDesc;
+    }
+    public double getItemCost(){
+        return itemCost;
+    }
+    public double getItemSalePrice(){
+        return itemSalePrice;
+    }
+
+    public double getItemProfit(){
+        double itemProfit = itemSalePrice - itemCost;
+        return itemProfit;
+    }
+    public double getItemMargin(){
+        double itemMargin = (itemProfit/itemSalePrice) * 100;
+        return itemMargin; 
+    }
+
+    public String toString(){
+        return "This is a " + itemName + " located in the " + itemDesc + " aisle. It has a sale price of $" + itemSalePrice;
     }
 }
