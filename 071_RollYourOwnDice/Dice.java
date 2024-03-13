57f798650a7caaa13434fae6ecf287a4e648e63e
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-03-13 14:43:54 -0500

    cqwsv

---
+++ 071_RollYourOwnDice/Dice.java
@@ -0,0 +1,16 @@
+public class Dice {
+    private int sides;
+
+    public Dice(int sides){
+        this.sides = sides;
+    } 
+
+    public void roll(){
+        int roll = (int)(1 + (Math.random() * sides));
+        return roll;
+    }
+
+    public getSides(){
+        return sides;
+    }
+}
