0d224d2872ec630f092f2d0ba4e93eb21450ba70
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-03-14 14:22:43 -0500

    ebrbfbebb

--- 071_RollYourOwnDice/AverageDiceRolls.java
+++ 071_RollYourOwnDice/AverageDiceRolls.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
 public class AverageDiceRolls {
-    
+    public static void main(String[] args) {
+       Dice diceOne = new Dice(6);
+       
+
+    }
 }
--- 071_RollYourOwnDice/Dice.java
+++ 071_RollYourOwnDice/Dice.java
@@ -1,13 +1,19 @@
 public class Dice {
     private int sides;
+    private int currentValue;
 
     public Dice(int sides){
         this.sides = sides;
+        roll();
     } 
 
-    public void roll(){
-        int roll = (int)(1 + (Math.random() * sides));
-        return roll;
+    //roll is not a variable, but a way to set a value for current value
+    public void roll() {
+        currentValue = (int)(1 + (Math.random() * sides));
+    }
+
+    public int getCurrentValue(){
+        return currentValue;
     }
 
     public int getSides(){
