c28fd444494d11f635f8201e5d6d6b3d0ce66017
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-03-15 11:28:47 -0500

    cs asaa adc Dg a

--- 071_RollYourOwnDice/AverageDiceRolls.java
+++ 071_RollYourOwnDice/AverageDiceRolls.java
@@ -2,8 +2,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class AverageDiceRolls {
     public static void main(String[] args) {
-        Scanner Scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         Dice diceOne = new Dice(6);
+        ArrayList<Integer[]> averageRoll = new ArrayList<>();
+        String userInput = "";
+        boolean userChoice = false;
+
+        System.out.println("Roll? y or n");
+        userInput = scan.nextLine();
+
+        if(userInput == "y"){
+            userChoice = true;
+            diceOne.roll();
+            System.out.println(diceOne.getCurrentValue());
+        }
+        else{
+            System.out.println("OK");
+        }
+
+
+
+
+
        
 
     }
