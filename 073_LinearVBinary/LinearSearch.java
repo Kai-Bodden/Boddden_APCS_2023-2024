e6126c637e474ec8e14d9ffd804b1b7afeeb2b06
Kai-Bodden <144169484+Kai-Bodden@users.noreply.github.com>
2024-04-08 21:46:23 -0500

    finished linear working on binary

--- 073_LinearVBinary/LinearSearch.java
+++ 073_LinearVBinary/LinearSearch.java
@@ -1,20 +1,36 @@
 public class LinearSearch{
 public int[] specialNumbers = {-8388, 9807, -4011, 5423, -4, 56, -990, 999};
     public static void main(String[] args) {
-        LinearSearch searchOne = new LinearSearch();
-        int correctIndex = searchOne.
+        //creating instance of my numbers
+        OneThousandSortedRandomNumbers numbers = new OneThousandSortedRandomNumbers();
 
+        //getting sorted array
+        int[] sortedNumbers = numbers.getSortedNumbers();
+
+        //starting my linear search instance
+        LinearSearch searchOne = new LinearSearch();
+
+        //linear search for all special numbers
+        for(int num : searchOne.specialNumbers){
+            int correctIndex = searchOne.linearSearch(sortedNumbers, num);
+            if(correctIndex != -1){
+                System.out.println("(Linear search) Special number " + num + " was found at index " + correctIndex);
+            }
+            else{
+                System.out.println("(Linear search) Special number " + num + " was not found in the array");
+            }
+        }
     
     }
-
+    //linear search method
     public int linearSearch(int[] arrToSearch , int target){
         for(int i = 0; i < arrToSearch.length; i++){
             System.out.println("This is the " + (i + 1) + " loop.");
             if(arrToSearch[i] == target){
-                return i;
+                return i; //what returns if the target is found
             }
         }
-        return -1;
+        return -1; //if target isnt found, return -1
     }
 }
 
