2c8abc57100da58057c30ef4c2a5c06affefab3f
Kai-Bodden <bodden.kai.24@c-dh.org>
2024-04-11 14:03:27 -0500

    started 074

---
+++ 074_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class SelectionSortSomeIntegers {
+    Scanner scan = new Scanner(System.in);
+
+    int userInput = 0;
+
+    System.out.println("How man integers do you want?");
+    
+}
