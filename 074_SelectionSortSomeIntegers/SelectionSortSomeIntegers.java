5bb7b5ad2a39bfba4ba890232c65fe4a28560250
Kai-Bodden <bodden.kai.24@c-dh.org>
2024-04-11 14:40:42 -0500

    working on 074

--- 074_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
+++ 074_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -1,10 +1,25 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class SelectionSortSomeIntegers {
+    public static void main(String[] args) { 
     Scanner scan = new Scanner(System.in);
 
     int userInput = 0;
-
     System.out.println("How man integers do you want?");
-    
+    userInput = scan.nextInt();
+
+
+
+    }
+
+    public static ArrayList<Integer> randomIntGenerator(int size){
+        ArrayList<Integer> randomInts = new ArrayList<>();
+        for(int i =0; i <size; i++){
+            randomInts.add((int) (Math.random() * 1000) + 1);
+        }
+        return randomInts;
+    }
+
+
+
 }
