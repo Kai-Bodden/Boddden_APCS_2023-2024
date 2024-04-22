edf7ea751252b06120dca6ebf9aed0e6272fc7ba
Kai-Bodden <bodden.kai.24@c-dh.org>
2024-04-22 14:39:34 -0500

    almost done

--- 077_WeatherComparison/WeatherComparison.java
+++ 077_WeatherComparison/WeatherComparison.java
@@ -1,4 +1,5 @@
 public class WeatherComparison {
+    private Double[][] weatherTemps;
     public static void main(String[] args) {
         Double[][] weatherTemps = {
             {62.59, 53.88, 34.85, 43.13, 44.13, 40.38, 39.71},
@@ -8,26 +9,47 @@ public class WeatherComparison {
             {37.4, 39.1, 40.5, 38.3, 42.0, 48.2, 55.7},
             {45.4, 53.0, 29.6, 31.8, 43.2, 50.7, 54.9},
         };
-        averageWeather(weatherTemps);
-      
+        WeatherComparison comparison = new WeatherComparison(weatherTemps);
+        comparison.averageWeather();
+        comparison.averageWeeklyWeather();
+        comparison.averageDailyWeather();
     }
 
-    public static void averageWeather(Double[][] weatherTemps){
+    public WeatherComparison(Double[][] weatherTemps) {
+        this.weatherTemps = weatherTemps;
+    }
+
+    public void averageWeather() {
         int count = 0;
         double sum = 0;
-        for(int row = 0; row < weatherTemps.length; row++){
-            for(int column = 0; column < weatherTemps[row].length; column++){
+        for (int row = 0; row < weatherTemps.length; row++) {
+            for (int column = 0; column < weatherTemps[row].length; column++) {
                 sum += weatherTemps[row][column];
                 count++;
             }
         }
-        System.out.println("The average temperature over the 6 year span is " + (sum/count) + " degrees.");
-        } 
+        System.out.println("The average temperature over the 6 year span is " + (sum / count) + " degrees.");
+    }
 
-    public static void averageWeeklyWeather(Double[][] weatherTemps){
-        double weekSum = 0;
-        for(int row = 0; row < weatherTemps.length; row++){
-            
+    public void averageWeeklyWeather() {
+        for (int week = 0; week < weatherTemps.length; week++) {
+            double weekSum = 0;
+            for (int day = 0; day < weatherTemps[week].length; day++) {
+                weekSum += weatherTemps[day][week];
+            }
+            double weekAverage = weekSum / weatherTemps[week].length;
+            System.out.println("The average temp for week " + (week + 1) + " was " + weekAverage);
+        }
+    }
+
+    public void averageDailyWeather() {
+        for (int day = 0; day < weatherTemps[0].length; day++) {
+            double dailySum = 0;
+            for (int week = 0; week < weatherTemps.length; week++) {
+                dailySum += weatherTemps[week][day];
+            }
+            double dailyAverage = dailySum / weatherTemps.length;
+            System.out.println("The average for April " + (day + 1) + " was " + dailyAverage);
         }
     }
 }
