a6de11df1003f48db20b835faca695b2441e6131
Kai-Bodden <bodden.kai.24@c-dh.org>
2024-05-21 13:58:07 -0500

    started

--- 081_PolymorphicPokemon/Pokemon.java
+++ 081_PolymorphicPokemon/Pokemon.java
@@ -4,5 +4,17 @@ public class Pokemon {
     private int spAtk;
     private int spDef;
     private int speed;
+
+    public Pokemon(int hp, int atk, int spAtk, int spDef, int speed){
+        this.hp = hp;
+        this.atk = atk;
+        this.spAtk = spAtk;
+        this.spDef = spDef;
+        this.speed = speed;
+    }
+
+    public void attack(){
+        
+    }
     
 }
